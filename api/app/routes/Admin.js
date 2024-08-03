21b572058e2f1518baa522e3382b039163d981dd
WebBonding21 <webbonding21@gmail.com>
2024-08-02 23:19:20 -0400

    Solving

--- api/app/routes/Admin.js
+++ api/app/routes/Admin.js
@@ -3,8 +3,11 @@
 const express = require('express');
 const router = express.Router();
 const {registrarAdmin, eliminarAdmin} = require('../controllers/Admin');
+const {login} = require('../controllers/LoginAdmin');
 
 router.post('/admin', registrarAdmin);
 router.delete('/admin/:id', eliminarAdmin);
+
+router.post('/admin-login', login);
 
 module.exports = router
